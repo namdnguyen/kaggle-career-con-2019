776b84e455958453104b43902317b7329723a269
Nam D. Nguyen <nnguyen83@gmail.com>
2019-04-08 10:52:04 -0500

    Add create dataset script

---
+++ src/data/create_dataset.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Creates test and training sets from Kaggle data sets.
+"""
+
+import pandas as pd
+
+train = pd.read_csv('../../data/raw/X_train.csv')
